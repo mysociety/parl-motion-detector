8c9ce7be85204e9c47e080c4562416c54e1276b7
Alex Parsons <alex.parsons@mysociety.org>
2024-09-06 15:45:04 +0000

    Complete agreement and division finders

---
+++ src/parl_motion_detector/agreements.py
@@ -0,0 +1,175 @@
+from __future__ import annotations
+
+import json
+from pathlib import Path
+from typing import (
+    Generic,
+    Protocol,
+    TypeVar,
+)
+
+from mysoc_validator import Transcript
+from mysoc_validator.models.transcripts import (
+    Division,
+    MajorHeading,
+    MinorHeading,
+    Speech,
+)
+from pydantic import BaseModel
+
+from parl_motion_detector.detector import PhraseDetector
+from parl_motion_detector.enum_helpers import StrEnum
+
+
+class Stringable(Protocol):
+    def __str__(self) -> str: ...
+
+
+class HasSpeechAndDate(BaseModel):
+    date: str
+    speech_id: str
+
+
+T = TypeVar("T", bound=HasSpeechAndDate)
+
+
+class Flag(StrEnum):
+    """
+    Store Flags for different kinds of motion processe
+    """
+
+    COMPLEX_MOTION = "complex_motion"
+    CLAUSE_MOTION = "clause_motion"
+    ASKED_IMMEDIATELY = "asked_immediately"
+
+
+class Agreement(HasSpeechAndDate):
+    date: str
+    major_heading_id: str
+    minor_heading_id: str
+    speech_id: str
+    paragraph_pid: str = ""
+    end_reason: str = ""
+    agreement_pid: str = ""
+    agreed_text: str
+    preceeding_text: str
+
+    def finish(self, collection: AgreementCollection, end_reason: str):
+        self.end_reason = end_reason
+        collection.motions.append(self)
+        return None
+
+
+class Collection(BaseModel, Generic[T]):
+    motions: list[T] = []
+
+    def basic_dict(self):
+        return {m.speech_id: str(m) for m in self.motions}
+
+    def dump_test_data(self, tests_data_path: Path):
+        debate_date = self.motions[0].date
+        with (tests_data_path / f"{debate_date}.json").open("w") as f:
+            json.dump(self.basic_dict(), f)
+
+
+AgreementCollection = Collection[Agreement]
+DivisionCollection = Collection[DivisionHolder]
+
+agreement_made = PhraseDetector(
+    criteria=[
+        "Question put and agreed to.",
+        "Question agreed to.",
+        "read the First and Second time, and added to the Bill.",
+    ]
+)
+
+
+alts = [
+    "question put and agreed to",
+    "main question accordingly put and agreed to",
+    "question put and agreed",
+    "question agreed to",
+    "Question put and agree d to",
+    "Main Question put accordingly and agreed to",
+    "Question put (Standing Order No. 23) and agreed to",
+    "Main Question, as amended, put and agreed to",
+    "Main Question, as amended, put forthwith and agreed to",
+    "Question agreed to.",
+    "read the First and Second time, and added to the Bill.",
+    "Brought up, read the First and Second time, and added to the Bill",
+    "Brought up, read the First Time and Second Time and added to the Bill",
+    "Question put and agreed to.",
+    "Question put (Standing Order No.23) and agreed to.",
+    "question put and agreed to",
+]
+
+
+class DivisionHolder(HasSpeechAndDate):
+    date: str
+    major_heading_id: str
+    minor_heading_id: str
+    speech_id: str  # actually the division id for these purposes
+    preceding_speech: str
+
+
+def get_divisions(transcript: Transcript, date_str: str) -> DivisionCollection:
+    previous = None
+    major_heading = None
+    minor_heading = None
+    collection = DivisionCollection()
+    for item in transcript.items:
+        if isinstance(item, MajorHeading):
+            major_heading = item
+            minor_heading = None
+        if isinstance(item, MinorHeading):
+            minor_heading = item
+        if isinstance(item, Division):
+            if isinstance(previous, Speech):
+                previous_speech = str(previous.items[-1])
+            else:
+                previous_speech = ""
+            current_division = DivisionHolder(
+                date=date_str,
+                major_heading_id=major_heading.id if major_heading else "",
+                minor_heading_id=minor_heading.id if minor_heading else "",
+                speech_id=item.id,
+                preceding_speech=previous_speech,
+            )
+            collection.motions.append(current_division)
+
+        previous = item
+
+    return collection
+
+
+def get_agreements(transcript: Transcript, date_str: str) -> AgreementCollection:
+    collection = AgreementCollection()
+
+    for transcript_group in transcript.iter_headed_speeches():
+        minor_heading_id = (
+            transcript_group.minor_heading.id if transcript_group.minor_heading else ""
+        )
+        major_heading_id = (
+            transcript_group.major_heading.id if transcript_group.major_heading else ""
+        )
+
+        for index, paragraph in enumerate(transcript_group.speech.items):
+            try:
+                previous_paragraph = str(transcript_group.speech.items[index - 1])
+            except IndexError:
+                previous_paragraph = ""
+
+            if agreement_made(paragraph):
+                current_agreement = Agreement(
+                    date=date_str,
+                    major_heading_id=major_heading_id,
+                    minor_heading_id=minor_heading_id,
+                    speech_id=transcript_group.speech.id,
+                    paragraph_pid=paragraph.pid or "",
+                    agreed_text=str(paragraph),
+                    preceeding_text=previous_paragraph,
+                )
+                current_agreement = current_agreement.finish(
+                    collection, "one line agreement"
+                )
+    return collection
